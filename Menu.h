91f208fbc1bcc542a2a1e77e4ce9f1477f278fa3
TblRka <92474441+TblRka@users.noreply.github.com>
2022-11-05 13:28:49 +0300

    menu + patch2

--- Menu.h
+++ Menu.h
@@ -3,20 +3,48 @@
 #include "Sorts.h"
 #include "Windows.h"
 
+void Timeing(int code)
+{
+
+}
+
 void Menu()
 {
-	int choise1;
-	std::cout << "1. Sequence sorting\n" << "2. Time testing\n" << "Enter: ";
-	std::cin >> choise1;
-	Sleep(100);
-	system("cls");
+	int choise1 = 0;
+	while (choise1 != 3)
+	{
+		std::cout << "1. Sequence sorting\n" << "2. Time testing\n" << "3. Escape\n" << "Enter: ";
+		std::cin >> choise1;
 
-	if (choise1 == 1)
-	{
-		std::cout << "\n";
+		Sleep(10);
+		system("cls");
+
+		switch (choise1)
+		{
+			case 1:
+				int choise21;
+				std::cout << "Choose datatype: \n" << "1. Int\n" << "2. Float\n" << "3. Student\n";
+				std::cin >> choise21;
+
+				Sleep(10);
+				system("cls");
+
+				break;
+		
+			case 2:
+				int choise22;
+				std::cout << "Choose sorts: \n" << "1. Bubble sort\n" << "2. Insert sort\n" << "3. Bin insert sort\n" << "4. Heap sort\n" << "5. Quick sort\n";
+				std::cout << "Enter a number consisting of digits, selected sorts:";
+				std::cin >> choise22;
+
+				Sleep(10);
+				system("cls");
+
+				Timeing(choise22);
+
+				break;
+
+		}
 	}
-	if (choise1 == 2)
-	{
-		std::cout << "\n";
-	}
+
 }
